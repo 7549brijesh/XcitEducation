9e10affe91c5925cc65ee70f66ec835509ab1ae3
JOGINDER571 <snshrma7@gmail.com>
2022-01-04 15:29:51 +0530

    intern-project

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -6,6 +6,7 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import './footer.css';
 const Footer = () => {
     return (
         <>
@@ -19,55 +20,55 @@ const Footer = () => {
                 
                 color="white" p ={5} m ={10}  
                 textAlign='center' >
-                    <Container maxWidth="lg">
+                    <Container maxWidth="lg" sx={{}}>
                         <Grid container spacing={5}>
                             <Grid item xs={12} sm = {3}>
                                 <Box borderBottom={1} mb={1} sx={{fontWeight:'bold'}} >Product</Box>
-                                <Box pb={0.8} ><Link href = "/"  color="inherit" >Internship In India</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Internship in Delhi</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Internship in Banglore</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Internship in Hyderabad</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Internship in Mumbai</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Internship in Chennai</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Internship in Gurugaon</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Internship in Kolkata</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">View Internship</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"  color="inherit" >Internship In India</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Internship in Delhi</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Internship in Banglore</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Internship in Hyderabad</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Internship in Mumbai</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Internship in Chennai</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Internship in Gurugaon</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Internship in Kolkata</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">View Internship</Link></Box>
                                 
                             </Grid>
                             <Grid item xs={12} sm = {3}>
                                 <Box borderBottom={1} mb={1}  sx={{fontWeight:'bold'}}>Internship by Stream</Box>
-                                <Box pb={0.8}><Link href ="/"  color="inherit">Computer Science Internship</Link></Box>
-                                <Box pb={0.8}><Link href ="/"  color="inherit">Electronics Internship</Link></Box>
-                                <Box pb={0.8}><Link href ="/"  color="inherit">Mechanical Internship</Link></Box>
-                                <Box pb={0.8}><Link href ="/"  color="inherit">Civil Internship</Link></Box>
-                                <Box pb={0.8}><Link href ="/"  color="inherit">Marketing Internship</Link></Box>
-                                <Box pb={0.8}><Link href ="/"  color="inherit">Chemical Internship</Link></Box>
-                                <Box pb={0.8}><Link href ="/"  color="inherit">Finance Internship</Link></Box>
-                                <Box pb={0.8}><Link href ="/"  color="inherit">Summer Research Fellowship</Link></Box>
-                                <Box pb={0.8}><Link href ="/"  color="inherit">Campus Ambassador Program</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"  color="inherit">Computer Science Internship</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"  color="inherit">Electronics Internship</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"  color="inherit">Mechanical Internship</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"  color="inherit">Civil Internship</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"  color="inherit">Marketing Internship</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"  color="inherit">Chemical Internship</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"  color="inherit">Finance Internship</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"  color="inherit">Summer Research Fellowship</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"  color="inherit">Campus Ambassador Program</Link></Box>
                             </Grid>
                             <Grid item xs={12} sm = {3}>
                                 <Box borderBottom={1} mb={1}  sx={{fontWeight:'bold'}}>Online Trainings</Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Programming with Python</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Digital Marketing</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Web Development</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Machine Learning</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Advanced Excel</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">AutoCAD</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Creative Writing</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Data Science</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Programming with Python</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Digital Marketing</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Web Development</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Machine Learning</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Advanced Excel</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">AutoCAD</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Creative Writing</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Data Science</Link></Box>
                             </Grid>
                             <Grid item xs={12} sm = {3} bp={5}>
                                 <Box borderBottom={1} mb={1}  sx={{fontWeight:'bold'}}>About Internhsala</Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">About Us</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">We're hiring</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Hire interns for your company</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Team Diary</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Blog</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Our Services</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Terms & Conditions</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Privacy</Link></Box>
-                                <Box pb={0.8}><Link href ="/"   color="inherit">Contact US</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">About Us</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">We're hiring</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Hire interns for your company</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Team Diary</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Blog</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Our Services</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Terms & Conditions</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Privacy</Link></Box>
+                                <Box className='hover' pb={0.8}><Link className='r-uline' href ="/"   color="inherit">Contact US</Link></Box>
                             </Grid>
                             
                         </Grid>
@@ -76,7 +77,7 @@ const Footer = () => {
                     
                     <Box pt= {3} >
                         <Divider style={{ background: 'white' }}/> 
-                        <container maxWidth = "lg">
+                        <Container maxWidth = "lg">
                      
                             <Grid container spacing={5}>
                     
@@ -96,7 +97,7 @@ const Footer = () => {
                     
                             </Grid>
 
-                        </container>
+                        </Container>
                     </Box>
                 </Box>
             </footer>
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -5,9 +5,10 @@ import { BrowserRouter,Route,Routes,Link } from "react-router-dom";
 import About from "../Pages/About";
 import Contact from "../Pages/Contact";
 import Home from "../Pages/Home";
+import Login from "../Login/Login";
 // import { makeStyles } from "@mui/styles";
 
-// const useStyles = makeStyles(theme =>({
+// const useStyles = makeStyles((theme) =>({
 //     sectionDesktop: {
 //         display: "none",
 //         [theme.breakpoints.up("md")]:{
@@ -16,44 +17,34 @@ import Home from "../Pages/Home";
 //     },
 // }));
 
-// const useStyles = makeStyles({
-//     root: {
-//       background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-//       border: 0,
-//       borderRadius: 3,
-//       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-//       color: 'white',
-//       height: 40,
-//       padding: '0 20px',
-//     },
-//   });
 
 const Navbar=() =>{
     // const classes = useStyles();
     return(
         <>
-            <BrowserRouter>
-                    <AppBar position="static">
-                        <Toolbar>
-                            <Typography variant="h6" style={{flexGrow:1}}>XcitEducation</Typography>
-                            {/* <div className={classes.sectionDesktop}> */}
-                            <div>
-                                <Button color="inherit" sx={{mr:2}} component={Link} to="/">Home</Button>
-                                <Button color="inherit" sx={{mr:2}}component={Link} to="/contact">Contact</Button>
-                                <Button color="inherit" sx={{mr:2}}component={Link} to="/about">About Us</Button>
-                                <Button variant="contained" sx={{mr:2}} color="success" endIcon={<LoginIcon />} component={Link} to="/login">Login</Button>
-                                <Button variant="contained" color="error" component={Link} to="/register">Register</Button>
-                            </div>
-                        </Toolbar>
-                    </AppBar>
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        <Route path="/about" element={<About/>}/>
-                        <Route path="/contact" element={<Contact/>}/>
-                        {/* <Route path="/login" element={<Login/>}/>
-                        <Route path="/register" element={<Register/>}/> */}
-                    </Routes>
-            </BrowserRouter>
+                <BrowserRouter>
+                        <AppBar position="static">
+                            <Toolbar>
+                                <Typography variant="h6" style={{flexGrow:1}}>XcitEducation</Typography>
+                                {/* <div className={classes.sectionDesktop}> */}
+                                <div>
+                                    <Button color="inherit" sx={{mr:2}} component={Link} to="/">Home</Button>
+                                    <Button color="inherit" sx={{mr:2}}component={Link} to="/contact">Contact</Button>
+                                    <Button color="inherit" sx={{mr:2}}component={Link} to="/about">About Us</Button>
+                                    <Button variant="contained" sx={{mr:2}} color="success" endIcon={<LoginIcon />} component={Link} to="/login">Login</Button>
+                                    <Button variant="contained" color="error" component={Link} to="/register">Register</Button>
+                                </div>
+                            </Toolbar>
+                        </AppBar>
+                        
+                        <Routes>
+                            <Route path="/" element={<Home/>}/>
+                            <Route path="/about" element={<About/>}/>
+                            <Route path="/contact" element={<Contact/>}/>
+                            <Route path="/login" element={<Login/>}/>
+                            {/* <Route path="/register" element={<Register/>}/> */}
+                        </Routes>
+                </BrowserRouter>
         </>
     )
 }
