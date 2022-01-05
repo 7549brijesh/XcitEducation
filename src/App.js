d247b0460bad9381fff6801f6d70a8ad5110b614
Brijesh Sundi <brijeshsundi@gmail.com>
2022-01-06 02:19:28 +0530

    register
    Register page has been added
    Tried to update the Login page(Failed to add the Routing)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
-import Footer from './Footer/Footer';
-import Navbar from './Navbar/Navbar';
+import "./App.css";
+import Footer from "./Footer/Footer";
+import Navbar from "./Navbar/Navbar";
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Footer/>
+      <Navbar />
+      <Footer />
     </>
-
   );
 }
 
---
+++ src/Register/CompanyReg.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+
+const CompanyReg = () => {
+    return (
+        <>
+            <Box py={20} textAlign="center">
+                <Typography variant='h1'>Company Registration Page!!</Typography>
+            </Box>
+        </>
+    )
+}
+
+export default CompanyReg
